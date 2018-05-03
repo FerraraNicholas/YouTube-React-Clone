d31e5d27ca600a4504c3be4546303b00e613e65f
Nicholas Ferrara <ferraranicholas@yahoo.com>
2018-05-02 23:46:06 -0400

    Add function callback to change state in parent component.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -13,12 +13,17 @@ class SearchBar extends Component {
         //Defines an anonymous function with an event parameter
         //That sets the state of the value of input field each time it has changed
         return (
-            <div>
+            <div className="search-bar">
                 <input 
                 value={this.state.term}
-                onChange={ event => this.setState({ term: event.target.value }) } />
+                onChange={event => this.onInputChange(event.target.value)} />
             </div>
         );
+    }
+
+    onInputChange(term) {
+        this.setState({term});
+        this.props.onSearchTermChange(term);
     }
 }
 
